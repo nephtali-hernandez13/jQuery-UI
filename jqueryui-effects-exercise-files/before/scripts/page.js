376fa6d40fd0a9e2d6daf6c01445ce30f2083b1b
Nephtali Abisai Hernández Olvera <sistemas.naho13@gmail.com>
2019-12-19 09:56:51 -0700

    Add jQuery UI

---
+++ jqueryui-effects-exercise-files/before/scripts/page.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="jquery-ui-1.8.16.custom.js" />
+
+$(function () {
+    
+    $("#target").css("position","relative")
+        .css("position", "relative")
+        .click(function() {
+            $(this).toggleClass("leftTarget","slow");
+        /*$(this).animate({
+            "width": "+=20",
+            "height": "+=20",
+            "background-color": "red"
+            left: 300
+        }, 1000, 'easeInOutElastic');*/
+    
+        /*$(this).effect("explode", { pieces: 16 }, function() {
+            alert("Done!")
+        });*/
+
+    });
+
+});
---
+++ jqueryui-interactions-exercise-files/before/scripts/page.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="jquery-ui-1.8.16.custom.js" />
+
+$( function(){
+    $("#draggables").children().draggable();
+    /*$("#d1").draggable({ revert: "invalid" });
+    $("#d2").draggable({ snap: "#d1, #d3", helper: function(){
+        return $("<div>I am a helper</div>");
+    } });
+    $("#d3").draggable({ handle: "header" });
+    $("#d1, #d2, #d3").draggable( "option", "stack", ".ui-draggable" );*/
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        drop: function (event, ui) {
+            ui.draggable.hide("explode");
+        }
+    });
+
+    $("#sortable").sortable({ axis: "y", placeholder: "placeholder" });
+    $("#d3").resizable({ aspectRatio:true, alsoResize: "#d2" });
+});
